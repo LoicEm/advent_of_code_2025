427cd136f1f8b706c5460bfa3e9122d9e3496daa
Lo <plsnospam@fastmail.com>
2025-12-29 14:38:23 +0100

    Add a selection of which job to run

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
+use enum_display::EnumDisplay;
+use inquire::error::InquireResult;
+use inquire_derive::Selectable;
+
+mod fifth_day;
 mod first_day;
 mod fourth_day;
 mod second_day;
 mod third_day;
 
-fn main() {
-    first_day::main();
-    second_day::main();
-    third_day::main();
-    fourth_day::main();
+#[derive(Debug, Clone, Copy, Selectable, EnumDisplay)]
+enum DayOption {
+    First,
+    Second,
+    Third,
+    Fourth,
+    Fifth,
+    Exit,
+}
+
+fn main() -> InquireResult<()> {
+    let mut prompt_for_selection = true;
+    while prompt_for_selection {
+        let day =
+            DayOption::select("Select the day over which to run the problem, or exit").prompt()?;
+
+        match day {
+            DayOption::First => first_day::main(),
+            DayOption::Second => second_day::main(),
+            DayOption::Third => third_day::main(),
+            DayOption::Fourth => fourth_day::main(),
+            DayOption::Fifth => fifth_day::main(),
+            DayOption::Exit => prompt_for_selection = false,
+        }
+    }
+    Ok(())
 }
